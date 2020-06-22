d2def40132505d415986de730be037bb9c7806de
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-06-21 21:26:51 -0300

    Initial Structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState} from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
+
+  const [productos, setProductos] = useState([
+    {id: 1, nombre: 'Camisa React', precio: 50},
+    {id: 2, nombre: 'Camisa Node', precio: 40},
+    {id: 3, nombre: 'Camisa Angular', precio: 30},
+    {id: 4, nombre: 'Camisa Vue', precio: 20}
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header 
+        title = 'Tienda Virtual'
+      />
+      <Footer 
+        year = {new Date().getFullYear()}
+      />
+    </Fragment>
   );
 }
 
